20fccf95f60d28bb3432a05aeef7fc0dbbf67d1d
Zacky171 <mmuzackyr9@gmail.com>
2025-12-23 00:59:11 +0700

    refactor configuration files for improved readability and consistency

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
-import adapter from '@sveltejs/adapter-cloudflare';
-
-/** @type {import('@sveltejs/kit').Config} */
-const config = { kit: { adapter: adapter() } };
-
-export default config;
+import adapter from "@sveltejs/adapter-cloudflare";
+export default {
+kit: {
+adapter: adapter()
+}
+};
